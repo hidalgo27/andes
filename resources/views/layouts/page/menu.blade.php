5fa99292b5e7a83682d5a72eb01dbc09e39b8dbb
Hidalgo <hidalgochpnce@gmail.com>
2018-03-22 16:28:00 -0500

    Se cambio de idioma

--- resources/views/layouts/page/menu.blade.php
+++ resources/views/layouts/page/menu.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-4 col-sm-2 position-relative">
                 <div class="logo-goto position-absolute w-100 top-0 p-3 border rounded border-dark bg-dark">
-                    <a href="/"><img src="{{asset('images/logo-goto-b.png')}}" alt="" class="img-fluid"></a>
+                    <a href="/"><img src="{{asset('images/logo-andes-b.png')}}" alt="" class="img-fluid"></a>
                 </div>
             </div>
             <div class="col col-sm">
--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -35,67 +35,82 @@
         </div>
     </section>
 
-    <section class="py-3 bg-white">
+    <!-- /. -->
+    <!-- Page Content -->
+    <section class="bg-white">
         <div class="container">
-            <div class="row">
+            <div class="row pb-5">
                 <div class="col text-center">
-                    <h1 class="text-g-yellow">QUIÉNES SOMOS Y POR QUÉ AMAMOS VIAJAR</h1>
-                    {{--<p class="h2 text-secondary pt-3">USTED TENDRÁ UNA EXPERIENCIA INIMAGINABLE</p>--}}
+                    <p class="h2 text-secondary pt-3">VOCÊ TERÁ A EXPERIÊNCIA DE UM PERUANO</p>
                     {{--<p class="h4"></p>--}}
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <p class="mb-0 lead text-secondary text-center"><b>En GOTOPERU creemos en la excelencia, seguridad, atención personalizada y calidad de los servicios, es nuestro compromiso para con nuestros pasajeros. Por eso, nos esforzamos en satisfacer las necesidades de nuestros clientes al máximo.</b></p>
+                    <blockquote class="blockquote text-center alert-primary py-3">
+                        <p class="mb-0">Em ANDESVIAGENS cremos na excelência, segurança, a atenção personalizada e a qualidade dos serviços é o nosso compromisso para com nossos turistas. Por isso nós, nos esforçamos em satisfazer as necessidades dos nossos clientes ao máximo.</p>
+                        {{--<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>--}}
+                    </blockquote>
+
                 </div>
 
             </div>
         </div>
     </section>
 
-    <section class="py-3 bg-white">
+    <section class="py-5 bg-white">
         <div class="container">
 
             <div class="row no-gutters align-items-center bg-light">
 
                 <div class="col-12 col-md p-4">
-                    <h3 class="align-middle">NUESTRO COMPROMISO</h3>
-                    <p class="align-middle">No dejar escapar la oportunidad de que nuestros turistas descubran los encantos de Perú y vivir una aventura inolvidable.</p>
+                    <h3 class="align-middle">NOSSO COMPROMISSO</h3>
+                    <p class="align-middle">Não deixe fugir esta oportunidade de descobrir os encantos de Perú e viver uma aventura inesquecível.</p>
                 </div>
 
-                <div class="col-12 col-md p-4">
-                    <h3 class="align-middle">MISIÓN</h3>
-                    <p class="align-middle">Ofrecer a nuestros clientes los mejores recursos peruanos para una aventura inolvidable.</p>
+
+                <div class="col-12 col-md-auto d-sm-none d-md-none d-lg-inline">
+                    <img src="{{asset('images/about/about12.jpg')}}" alt="" class="img-fluid">
                 </div>
             </div>
             <div class="row w-100 no-gutters align-items-center bg-light">
+                <div class="col-12 col-md-auto d-none d-sm-inline">
+                    <img src="{{asset('images/about/about28.jpg')}}" alt="" class="img-fluid d-sm-none d-md-none d-lg-inline">
+                </div>
 
+
+                <div class="col-12 col-md-auto">
+                    <img src="{{asset('images/about/about6.jpg')}}" alt="" class="img-fluid">
+                </div>
 
                 <div class="col-12 col-md">
-                    <img src="{{asset('images/about/about-l.jpg')}}" alt="" class="img-fluid">
+                    <div class="border-0 p-2 text-center">
+                        <h3 class="text-primary">MISSÃO</h3>
+                        <p>Oferecer aos nossos clientes os melhores recursos Peruanos para uma aventura inesquecível.</p>
+                    </div>
                 </div>
             </div>
             <div class="row w-100 no-gutters align-items-center bg-dark">
                 <div class="col text-white text-center">
                     <div class="row justify-content-center">
-                        <div class="col-2 my-3">
-                            <img src="{{asset('images/logo-goto-b.png')}}" alt="" class="img-fluid">
+                        <div class="col-12 my-3">
+                            <img src="{{asset('images/logos/logo-andes-b.png')}}" alt="" class="img-fluid">
                         </div>
                     </div>
                     <div class="col-12">
-                        <p>La filosofía de GOTOPERU se creó de un principio muy simple: "Hacer todas las cosas correctas, tanto para nuestros pasajeros así con nuestro equipo."</p>
+                        <p>A filosofia da Andes Viagens é criada de um princípio muito simples:“Fazer todas as coisas certas, tanto para nossos passageiros, nossa equipe, como para os lugares em que passarmos.”</p>
                     </div>
                 </div>
             </div>
-            <div class="row bg-g-yellow no-gutters">
+            <div class="row bg-warning no-gutters">
                 <div class="col p-5 text-center">
-                    <h3>Andes Viajes fue fundada con el fin de hacer un sueño personal:</h3>
-                    <p>"Proveer inolvidables y diferentes experiencias para su viaje al Perú, proporcionando un servicio de primera clase para toda la comunidad internacional. Y con ello, generar nuevas oportunidades de empleo local en el área de turismo en Perú.</p>
+                    <h3>Andes Viagens foi fundada com a finalidade de fazer um sonho pessoal:</h3>
+                    <p>“Prover inesquecíveis e diferentes experiências para sua viagem ao Peru, fornecendo um serviço de 1º classe para toda a comunidade internacional. E com isso, gerar novas oportunidades de emprego local na área de turismo no Peru.”</p>
                 </div>
             </div>
             <div class="row no-gutters align-items-center bg-light">
                 <div class="col-12 col-md p-5">
-                    <p class="lead font-weight-normal">Ayudar a los pasajeros a elegir el viaje perfecto a la tierra de los Incas, con el fin de que tengan una hermosa experiencia, desde los Andes hasta el Amazonas, saldremos de nuestra rutina para asegurar una experiencia de viaje diferente, todo lo que usted imaginó.</p>
+                    <p class="lead font-weight-normal">Ajudar a nosos passageiros a escolher a viagem perfeita á terra dos Incas, com a finalidade de viver a vida dos estrangeiros, desde os Andes até o Amazonas, sairemos da nossa rutina para assegurar uma experiência de viagem diferente, todo o que voce imaginou.</p>
                 </div>
 
                 <div class="col-12 col-md-auto">
@@ -114,11 +129,11 @@
                 <div class="col text-white text-justify">
                     <div class="row justify-content-center">
                         <div class="col-3 mb-4">
-                            <img src="{{asset('images/logo-goto-b.png')}}" alt="" class="img-fluid">
+                            <img src="{{asset('images/logos/logo-andes-b.png')}}" alt="" class="img-fluid">
                         </div>
                     </div>
                     <div class="col">
-                        <p class="lead">"GOTOPERU" fue fundada para realizar un sueño personal: "Proporcionar experiencia de primera clase a la comunidad internacional, y generando oportunidades de empleo a nivel local". Es sumamente gratificante emplear y premiar a personas que demuestran esfuerzo, entusiasmo y compromiso de compartir lo mejor de Perú y América del Sur con el resto del mundo. "GOTOPERU" se ha convertido en una familia para todos nosotros. La mejor cosa de trabajar en los viajes de los Andes es que me obliga a estar conectado permanentemente con el mundo del arte, la cultura, la arqueología, la geografía, la conservación de la vida silvestre, y la cocina. Por esta razón, esto me ayuda a entender más del mundo de los viajes y formular nuevos métodos que garantizan memorables experiencias de viaje de nuestros clientes. Es maravilloso interactuar con personas increíbles de todo el mundo a diario.</p>
+                        <p class="lead">“Andes Viagens” foi fundada para realizar um sonho pessoal: "Proporcionar experiência de primeira classe pela comunidade internacional, e gerando oportunidades de emprego a nível local". É extremamente gratificante empregar e premiar pessoas que estremam esforço, entusiasmo e compromisso de compartilhar o melhor do Peru e da América do Sul com o resto do mundo. “Andes Viagens” tornou-se uma família para todos nós. A melhor coisa de trabalhar em Andes Viagens é que isso me obriga a estar permanentemente conectado com o mundo da arte, cultura, arqueologia, geografia, conservação da vida selvagem, e cozinha. Por esta razão, isso me ajuda a entender mais do mundo das viagens e formular novos métodos que garantem memorável experiências de viagem de nossos clientes. E maravilhoso interagir com pessoas incríveis de todo o mundo diariamente, já que me ensina mais de sua cultura.</p>
                     </div>
                 </div>
 
@@ -128,7 +143,7 @@
                     <div class="card text-white">
                         <img class="card-img-top" src="{{asset('images/about/about-o.jpg')}}" alt="Card image cap">
                         <div class="card-body text-center bg-dark">
-                            <p class="card-text"><span class="h4">PAUL CATANO</span> <small class="d-block">Director & Fundador</small></p>
+                            <p class="card-text"><span class="h4">PAUL CATANO</span> <small class="d-block">Diretor & Fundador</small></p>
                         </div>
                     </div>
                 </div>
@@ -136,7 +151,7 @@
                     <div class="card bg-light">
                         <img class="card-img-top" src="{{asset('images/about/about-u.jpg')}}" alt="Card image cap">
                         <div class="card-body text-center">
-                            <p class="card-text"><span class="h4">MARTIN CATANO</span> <small class="d-block">Director General</small></p>
+                            <p class="card-text"><span class="h4">MARTIN CATANO</span> <small class="d-block">Diretor Geral</small></p>
                         </div>
                     </div>
                 </div>
