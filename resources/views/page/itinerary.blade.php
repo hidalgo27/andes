bc307c2e2c09b718dfd8636393a280aadbf1482e
Hidalgo <hidalgochpnce@gmail.com>
2019-02-04 16:29:13 -0500

    Se corrigio errores de produccion

--- resources/views/page/itinerary.blade.php
+++ resources/views/page/itinerary.blade.php
@@ -93,10 +93,10 @@
                     @foreach($paquete_iti as $paquete_i)
                         {{--@if($paquete_i->duracion == 1)--}}
                             {{--<h1 class="font-montserrat text-g-green font-weight-bold">PACOTES NO PERU {{$paquete_i->duracion}} DIA: {{strtoupper(str_replace('-', ' ', $title))}}</h1>--}}
-                            {{--@php $s_none = 'd-none'; @endphp--}}
+                            @php $s_none = 'd-none'; @endphp
                         {{--@else--}}
                             {{--<h1 class="font-montserrat text-g-green font-weight-bold">PACOTES NO PERU {{$paquete_i->duracion}} DIAS: {{strtoupper(str_replace('-', ' ', $title))}}</h1>--}}
-                            {{--@php $s_none = ''; @endphp--}}
+                            @php $s_none = ''; @endphp
                         {{--@endif--}}
                     @endforeach
 
