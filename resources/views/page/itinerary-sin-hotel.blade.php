e81d1c84acae17563f0b3219216855131fdca619
hidalgo27 <hidalgoChPnce@gmail.com>
2019-03-26 14:42:15 -0500

    Update itinerary-sin-hotel.blade.php

--- resources/views/page/itinerary-sin-hotel.blade.php
+++ resources/views/page/itinerary-sin-hotel.blade.php
@@ -90,7 +90,7 @@
         <div class="container">
             <div class="row">
                 <div class="col text-center">
-                    @foreach($paquete_iti as  )
+                    @foreach($paquete_iti as  $paquete_i)
                         @if($paquete_i->duracion == 1)
                             <h1 class="font-montserrat text-g-green font-weight-bold">PACOTES NO PERU {{$paquete_i->duracion}} DIA: {{strtoupper(str_replace('-', ' ', $title))}}</h1>
                             @php $s_none = 'd-none'; @endphp
