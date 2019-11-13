7158039c7e053426462ee90e5e86d0df3bb75741
hidalgo <hidalgochpnce@gmail.com>
2019-11-13 11:22:30 -0500

    Create sirena whatsapp

--- resources/views/layouts/page/default.blade.php
+++ resources/views/layouts/page/default.blade.php
@@ -23,6 +23,7 @@
 
         gtag('config', 'UA-57960909-3');
     </script>
+    <script type="text/javascript" src="https://widget.sirena.app/get?token=zdF9nfRDq1GYnzBarfuv3NTwoEBBmEga" async="true"></script>
 
 </head>
 <body data-spy="scroll" data-target="#menu" class="position-relative">
@@ -666,9 +667,9 @@
     </script>
 
     <!-- begin olark code -->
-<script type="text/javascript" async> ;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
-/* custom configuration goes here (www.olark.com/documentation) */
-olark.identify('9427-560-10-8022');</script>
+{{--<script type="text/javascript" async> ;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");--}}
+{{--/* custom configuration goes here (www.olark.com/documentation) */--}}
+{{--olark.identify('9427-560-10-8022');</script>--}}
 <!-- end olark code -->
 
 
